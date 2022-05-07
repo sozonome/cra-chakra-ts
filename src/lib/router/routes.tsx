f2f02a7939c40654ef355d49d9cbf45a6dfaeba5
Agustinus Nathaniel <berserk.nate@gmail.com>
2022-05-07 14:45:14 +0700

    feat: add 404 page

--- src/lib/router/routes.tsx
+++ src/lib/router/routes.tsx
@@ -1,10 +1,15 @@
 import type { PathRouteProps } from 'react-router-dom';
 
+import Page404 from 'lib/pages/404';
 import HomePage from 'lib/pages/home';
 
 export const routes: Array<PathRouteProps> = [
   {
     path: '/',
     element: <HomePage />,
+  },
+  {
+    path: '*',
+    element: <Page404 />,
   },
 ];
