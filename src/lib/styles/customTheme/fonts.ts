82ca52e1ea7748d492021495d58586d77957abc4
Agustinus Nathaniel <berserk.nate@gmail.com>
2022-05-21 13:35:47 +0700

    refactor(theme): re-organize theme folder and imports

--- src/lib/styles/customTheme/fonts.ts
+++ src/lib/styles/customTheme/fonts.ts
@@ -1,8 +1,6 @@
 import type { DeepPartial, Theme } from '@chakra-ui/react';
 
-const fonts: DeepPartial<Theme['fonts']> = {
+export const fonts: DeepPartial<Theme['fonts']> = {
   body: 'sans-serif',
   heading: 'sans-serif',
 };
-
-export default fonts;
